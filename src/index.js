42ab9ed8a5ea406c96faa58213113d221b375cde
DESKTOP-D1QQ3CS\rayhs <ray.hsu@artsolutions.io>
2026-07-15 15:45:06 +0800

    finish muti-turn conversation

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const { GoogleGenAI } = require('@google/genai');
 const { app, BrowserWindow , ipcMain} = require('electron');
 const path = require('node:path');
 const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
+const chat = ai.chats.create({ model: 'gemini-3.5-flash' });
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) {
   app.quit();
@@ -52,30 +53,16 @@ app.on('window-all-closed', () => {
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and import them here.
 
-// ipcMain.handle('talk', async (event, message) => {
-//   // 這裡可以呼叫外部 API、讀寫檔案等等
-//   return '成功透過IPC talk';
-// });
-
-// ipcMain.handle('talk', async (event, message) => {
-//   try {
-//     const response = await ai.models.generateContent({
-//       model: 'gemini-3.5-flash',
-//       contents: message,
-//     });
-//     return response.text;
-//   } catch (error) {
-//     console.error('呼叫 Gemini API 時發生錯誤:', error);
-//     return '抱歉，發生錯誤，請稍後再試。';
-//   }
-// });
 
 ipcMain.handle('talk', async (event, message) => {
   try{
-    const response = await ai.models.generateContentStream({
-      model: 'gemini-3.5-flash',
-      contents: message,
-    });
+    // const response = await ai.models.generateContentStream({
+    //   model: 'gemini-3.5-flash',
+    //   contents: message,
+    // });
+    const response = await chat.sendMessageStream({
+      message: message,
+    })
 
     for await (const chunk of response) {
       event.sender.send('chunk', chunk.text);
